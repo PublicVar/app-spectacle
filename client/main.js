e9e426c83086506e2e6fd3bcdebc4a9650f1c905
PublicVar <dev.publicvar@gmail.com>
2017-03-23 11:33:52 +0400

    Add iron:router and home page

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import '../import/startup/account-config.js';
-import './main.html';
+
+// import '../import/startup/client/account-config.js';
+// import '../import/startup/client/routes.js';
+// import './main.html';
 
